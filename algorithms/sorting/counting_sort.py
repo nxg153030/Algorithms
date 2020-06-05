bf01d2734c3fd3349db5a00432e9ed20eba3bc1d
Nishant Gurrapadi <nishant.liteteesko@gmail.com>
2020-06-04 22:56:23 -0600

    repurpose sorting algorithms

--- algorithms/sorting/counting_sort.py
+++ algorithms/sorting/counting_sort.py
@@ -1,6 +1,3 @@
-from math import ceil, log2
-
-
 def counting_sort(A):
     """
     Counting sort assumes that the input consists on integers in a small range.
@@ -23,55 +20,8 @@ def counting_sort(A):
     A[:] = B
 
 
-def max_subarray(arr):
-    max_sum = arr[0]
-    temp_sum = max_sum
-    for i in range (1, len(arr)):
-        temp_sum = max(arr[i], arr[i] + temp_sum)
-        max_sum = max(temp_sum, max_sum)
-
-    return max_sum
-
-
-def searchRange(nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    if not nums:
-        return [-1, -1]
-
-    def bisect_left(nums, target):
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = (l + r) // 2
-            if nums[m] < target:
-                l = m + 1
-            else:
-                r = m
-        return l if nums[l] == target else -1
-
-    def bisect_right(nums, target):
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = (l + r) // 2
-            if nums[m] > target:
-                r = m - 1
-            else:
-                l = m
-        return l if nums[l] == target else -1
-
-    return [bisect_left(nums, target), bisect_right(nums, target)]
-
-
 if __name__ == '__main__':
-    # arr = [2, 5, 3, 0, 2, 3, 0, 3]
+    arr = [2, 5, 3, 0, 2, 3, 0, 3]
     # A = [100, 40, 24, 34, 33, 12, 17]
-    # counting_sort(arr)
-    # print(arr)
-    arr = [5, 5, 5, 7, 7, 7, 7, 8, 8, 8, 10]
-    # print(max_subarray(arr))
-    # print(searchRange(arr, target=5))
-    n = 3
-    print(getMoneyAmountdp(n))
+    counting_sort(arr)
+    print(arr)

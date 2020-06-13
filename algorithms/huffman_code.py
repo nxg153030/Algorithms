78568b3435fc2694912e0bbe269266d8e54304a0
Nishant Gurrapadi <nishant.liteteesko@gmail.com>
2020-06-13 15:46:13 -0600

    progress on huffman code implementation, implemented Character class with custom comparison functions

--- algorithms/huffman_code.py
+++ algorithms/huffman_code.py
@@ -1,18 +1,46 @@
-from datastructures import Heap, PriorityQueue
+from datastructures import PriorityQueue
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
 
 
 class Character:
-    def __init__(self, freq: int):
+    def __init__(self, key: str = '', freq: int = 0):
+        self.key = key
         self.freq = freq
+
+    def __lt__(self, node):
+        return self.freq < node.freq
+
+    def __gt__(self, node):
+        return self.freq > node.freq
+
+    def __str__(self):
+        return f'Character: {self.key}, freq: {self.freq}'
 
 
 def huffman(C):
     """
-    :param C: set of characters
+    :param C: list of characters
     :return:
     """
     n = len(C)
-    queue = PriorityQueue(list(C))
+    queue = PriorityQueue(C) # create a leaf node for each character, and add them to the priority queue
     queue.build_min_heap()
     for i in range(0, n-1):
-        new_node =
+        new_node = Character()
+        new_node.left = queue.heap_extract_min()
+        new_node.right = queue.heap_extract_min()
+        new_node.freq = new_node.left.freq + new_node.right.freq
+        queue.min_heap_insert(new_node)
+
+    return queue.heap_extract_min()
+
+
+if __name__ == '__main__':
+    # TODO: figure out how to pass the list of characters into the algorithm
+    # TODO: What am i trying to output?
+    l = [Character(key='a', freq=5), Character(key='b', freq=10), Character(key='c', freq=15), Character(key='d', freq=4)]
+    print(huffman(l))
--- algorithms/sorting/topological_sort.py
+++ algorithms/sorting/topological_sort.py
@@ -5,5 +5,17 @@ def topological_sort(G):
     depth_first_search(G)
 
 
+def canJump(nums):
+    _max = 0
+    for i, num in enumerate(nums):
+        if i > _max:
+            return False
+        _max = max(_max, i+num)
+    return True
+
+
 if __name__ == '__main__':
-    pass
+    l = [3, 2, 1, 0, 4]
+    l = [2, 3, 1, 1, 4]
+    l = [2, 3, 1, 1, 0, 4]
+    print(canJump(l))

0a01342c8996082f8e4944e6ede706440002932d
Nishant Gurrapadi <nishant.liteteesko@gmail.com>
2021-08-29 15:40:05 -0600

    Add recursive codeword generator for huffman code

--- algorithms/huffman_code.py
+++ algorithms/huffman_code.py
@@ -1,4 +1,5 @@
 from datastructures import PriorityQueue
+from collections import deque
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -7,8 +8,10 @@ LOGGER.setLevel(logging.DEBUG)
 
 
 class Character:
-    def __init__(self, key: str = '', freq: int = 0):
+    def __init__(self, key: str = '', freq: int = 0, left=None, right=None):
         self.key = key
+        self.left = left
+        self.right = right
         self.freq = freq
 
     def __lt__(self, node):
@@ -50,7 +53,33 @@ def huffman(characters):
     return queue.heap_extract_min()
 
 
+def has_path(root, codeword_arr, codeword, key):
+    if not root:
+        return False
+
+    codeword_arr.append(codeword)
+
+    if root.key == key:
+        return True
+
+    if has_path(root.left, codeword_arr, '0', key) or has_path(root.right, codeword_arr, '1', key):
+        return True
+
+    codeword_arr.pop(-1)
+    return False
+
+
+def get_codeword(root, key):
+    codeword_arr = []
+    codeword = ''
+    if has_path(root, codeword_arr, codeword, key):
+        return ''.join(codeword_arr)
+
+
 if __name__ == '__main__':
     l = [Character(key='f', freq=5), Character(key='e', freq=9), Character(key='c', freq=12),
          Character(key='b', freq=13), Character(key='d', freq=16), Character(key='a', freq=45)]
-    print(huffman(l))
+
+    _root = huffman(l)
+    huffman_code = get_codeword(_root, 'b')
+    print(huffman_code)
